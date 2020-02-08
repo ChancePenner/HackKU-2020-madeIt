4b997104a838316a325716eccad4c1d7e17ff0be
Chance Penner <chance.penner2016@gmail.com>
2020-02-08 16:51:33 -0600

    Add locations

--- MainProject/MainProject/MapVC.swift
+++ MainProject/MainProject/MapVC.swift
@@ -13,6 +13,12 @@ import GooglePlaces
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var txtSearch: UITextField!
+    
+    
+    @IBAction func locationTapped(_ sender: Any) {
+        gotoPlaces()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +29,12 @@ class MapVC: UIViewController {
         
     }
     
+    func gotoPlaces() {
+        txtSearch.resignFirstResponder()
+        let acController = GMSAutocompleteViewController()
+        acController.delegate = self
+        present(acController, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
@@ -43,4 +55,46 @@ extension MapVC: CLLocationManagerDelegate {
         print("locations = \(locValue.latitude) \(locValue.longitude)")
        // lblocation.text = "latitude = \(locValue.latitude), longitude = \(locValue.longitude)"
     }
+}
+
+extension MapVC: GMSAutocompleteViewControllerDelegate {
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+         print("Place name: \(String(describing: place.name))")
+       dismiss(animated: true, completion: nil)
+       
+       self.mapView.clear()
+       self.txtSearch.text = place.name
+       
+       /*
+       let placeGmap = GoogleMapObjects()
+       placeGmap.lat = place.coordinate.latitude
+       placeGmap.long = place.coordinate.longitude
+       placeGmap.address = place.name*/
+       
+       //self.delegate?.getThePlaceAddress(vc: self, place: placeGmap, tag: self.FieldTag)
+   
+       let cord2D = CLLocationCoordinate2D(latitude: (place.coordinate.latitude), longitude: (place.coordinate.longitude))
+       
+       let marker = GMSMarker()
+       marker.position =  cord2D
+       marker.title = "Location"
+       marker.snippet = place.name
+       
+       let markerImage = UIImage(named: "icon_offer_pickup")!
+       let markerView = UIImageView(image: markerImage)
+       marker.iconView = markerView
+       marker.map = self.mapView
+       
+       self.mapView.camera = GMSCameraPosition.camera(withTarget: cord2D, zoom: 15)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        
+    }
+    
+    
 }
