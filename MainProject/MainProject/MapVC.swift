bfbed8eaa475791bc1c8b429bcd8ebd23c66301f
Markus Becerra <markusbecerra@ku.edu>
2020-02-09 00:12:25 -0600

    Tryingtoimplement HTTPS request with Alamofire for json object

--- MainProject/MainProject/MapVC.swift
+++ MainProject/MainProject/MapVC.swift
@@ -9,10 +9,16 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import Alamofire
+
 
 class MapVC: UIViewController {
  
-
+var address = "haha"
+var pincode = "haha"
+var state = "haha"
+var city = "haha"
+var country = "haha"
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var txtSearch: UITextField!
     
@@ -83,6 +89,7 @@ extension MapVC: GMSAutocompleteViewControllerDelegate {
 print("Place Latitude: \(place.coordinate.latitude)")
 print("Place Longitude: \(place.coordinate.longitude)")
         //DEBUG SECTION
+        getAddressFromLatLong(latitude: (place.coordinate.latitude), longitude: (place.coordinate.longitude))
 
        let cord2D = CLLocationCoordinate2D(latitude: (place.coordinate.latitude), longitude: (place.coordinate.longitude))
         
@@ -110,5 +117,47 @@ print("Place Longitude: \(place.coordinate.longitude)")
         dismiss(animated: true, completion: nil)
     }
     
+    func getAddressFromLatLong(latitude: Double, longitude : Double) {
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=\(latitude),\(longitude)&key=AIzaSyCQJO6u77UF8FLdqBps0JzA0jjbBdkLuWI"
+
+        Alamofire.request(url).validate().responseJSON { response in
+            switch response.result {
+            case .success:
+
+                let responseJson = response.result.value! as! NSDictionary
+
+                if let results = responseJson.object(forKey: "results")! as? [NSDictionary] {
+                    if results.count > 0 {
+                        if let addressComponents = results[0]["address_components"]! as? [NSDictionary] {
+                            self.address = results[0]["formatted_address"] as? String ?? ""
+                            for component in addressComponents {
+                                if let temp = component.object(forKey: "types") as? [String] {
+                                    if (temp[0] == "postal_code") {
+                                        self.pincode = component["long_name"] as? String ?? ""
+                                    }
+                                    if (temp[0] == "locality") {
+                                        self.city = component["long_name"] as? String ?? ""
+                                    }
+                                    if (temp[0] == "administrative_area_level_1") {
+                                        self.state = component["long_name"] as? String ?? ""
+                                    }
+                                    if (temp[0] == "country") {
+                                        self.country = component["long_name"] as? String ?? ""
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        print(address)
+        print(pincode)
+        print(city)
+        print(state)
+        print(country)
+    }
     
 }
